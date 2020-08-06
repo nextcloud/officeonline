a51640d79397a79512b3ed90aeb0d9e84dfc85f9
Julius Härtl <jus@bitgrid.net>
2020-08-06 08:42:23 +0200

    Use viewer by default
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- src/viewer.js
+++ src/viewer.js
@@ -3,14 +3,6 @@ import Office from './view/Office'
 const supportedMimes = 	OC.getCapabilities().officeonline.mimetypes.concat(OC.getCapabilities().officeonline.mimetypesNoDefaultOpen)
 
 document.addEventListener('DOMContentLoaded', function(event) {
-	// Only use it outside the files app for now
-	if (typeof OCA !== 'undefined'
-		&& typeof OCA.Files !== 'undefined'
-		&& typeof OCA.Files.fileActions !== 'undefined'
-	) {
-		return
-	}
-
 	if (OCA.Viewer) {
 		OCA.Viewer.registerHandler({
 			id: 'officeonline',
