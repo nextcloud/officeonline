4d55ddd3f80b5fd205a5f9fbb6d852e23c9598b9
Julius Härtl <jus@bitgrid.net>
2022-12-08 11:39:52 +0100

    Fix templates
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- appinfo/app.php
+++ appinfo/app.php
@@ -50,9 +50,9 @@ $eventDispatcher->addListener(
 if (class_exists('\OC\Files\Type\TemplateManager')) {
 	$manager = \OC_Helper::getFileTemplateManager();
 
-	$manager->registerTemplate('application/vnd.openxmlformats-officedocument.wordprocessingml.document', dirname(__DIR__) . '/assets/docxtemplate.docx');
-	$manager->registerTemplate('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', dirname(__DIR__) . '/assets/xlsxtemplate.xlsx');
-	$manager->registerTemplate('application/vnd.openxmlformats-officedocument.presentationml.presentation', dirname(__DIR__) . '/assets/pptxtemplate.pptx');
+	$manager->registerTemplate('application/vnd.openxmlformats-officedocument.wordprocessingml.document', dirname(__DIR__) . '/assets/template.docx');
+	$manager->registerTemplate('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', dirname(__DIR__) . '/assets/template.xlsx');
+	$manager->registerTemplate('application/vnd.openxmlformats-officedocument.presentationml.presentation', dirname(__DIR__) . '/assets/template.pptx');
 	$manager->registerTemplate('application/vnd.oasis.opendocument.presentation', dirname(__DIR__) . '/assets/template.odp');
 	$manager->registerTemplate('application/vnd.oasis.opendocument.text', dirname(__DIR__) . '/assets/template.odt');
 	$manager->registerTemplate('application/vnd.oasis.opendocument.spreadsheet', dirname(__DIR__) . '/assets/template.ods');
